5adf92e92a9ef5ec6ed21521724ea4d2ae074da0
LavrinDmitrovich <dmitrylavrinovich@gmail.com>
2023-05-04 13:57:53 +0300

    11 ready

--- 10. profit/Program.cs
+++ 10. profit/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace _10._profit
 {
@@ -6,7 +7,27 @@ namespace _10._profit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = int.Parse(Console.ReadLine());
+            int b = int .Parse(Console.ReadLine());
+            int c = int .Parse(Console.ReadLine());
+
+            int d = int .Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 0; i <= a; i++) 
+            {
+                for (int j = 0; j <= b; j++) 
+                {
+                    for (int k = 0; k <= c; k++) 
+                    {
+                        if (i + j * 2 + k *5 == d) 
+                            
+                        Console.WriteLine($"{i} * 1 lv. + {j} * 2 lv. + {k} * 5 lv. = {d} lv.")    ; 
+                    }
+                }
+                
+            }
         }
     }
 }
---
+++ 11. happy cat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace _11._happy_cat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+
+            double cost = 0;
+            double total =0;
+
+            for (int i = 1; i <= days; i++) 
+            {
+                for (int j = 1; j <= hours; j++)
+                {
+                    if ((i % 2 == 0) && ((j + 1) % 2 == 0)) { cost += 2.5; }
+                    if (((i + 1) % 2 == 0) && (j % 2 == 0)) { cost += 1.25; }
+
+                    if ((i % 2 == 0) && (j % 2 == 0)) { cost += 1; }
+                    if (((i + 1) % 2 == 0) && ((j+1) % 2 == 0)) { cost += 1; }
+
+
+                }
+
+                Console.WriteLine($"Day: {i} - {cost:f2} leva"); total += cost; cost= 0; 
+            }
+            Console.WriteLine($"Total: {total:f2} leva");
+            
+        }
+    }
+}
