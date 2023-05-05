9cfcf262a9ef396900236d8d0968aea61aabafc6
LavrinDmitrovich <dmitrylavrinovich@gmail.com>
2023-05-05 11:30:45 +0300

    14 ready

---
+++ 13. prime pairs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _13._prime_pairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            bool first = false;
+            bool second = false;
+
+            for (int i = a; i <= a+c; i++) 
+            {
+                for (int j = b; j <= b+d; j++) 
+                {
+                
+                    for (int k = 2;  k < j; k++) { if ((double)j % k == 0) first = true; }
+                    for (int l = 2; l < i; l++) { if ((double)i % l == 0) second = true; }
+
+                    
+
+                    if (first == false && second == false) { Console.WriteLine($"{i}{j}"); }
+                    else { first = false; second = false; }
+                }
+            }
+        }
+    }
+}
---
+++ 14. password gen/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _14._password_gen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < a; i++) 
+            {
+                for (int j = 1; j < a; j++) 
+                {
+                    for (int k = 97; k <97 + b; k++) 
+                    {
+                        for (int l = 97; l < 97 + b; l++) 
+                        {
+                           int s = 0;
+                            if (i>j)  s = i;
+                            else s = j;
+
+                            for (int m = s+1; m <= a; m++) { Console.Write($"{i}{j}{(char)k}{(char)l}{m} "); }
+                        }
+                    }
+                } 
+            }
+        }
+    }
+}
